419fa96aa303376ad97526e50808a5d6aa7fbb3c
janmurin2 <janmurin2@gmail.com>
2020-06-20 15:14:01 +0200

    results for 10311

--- Models/ResultsDatabase.cs
+++ Models/ResultsDatabase.cs
@@ -148,11 +148,11 @@ namespace SeleniumResults.Models
                 Console.WriteLine(or);
             }
 
-            var stats = orderedResults.First(x => x.Name == "JournalFromCaseSmokeTest");
+            var stats = orderedResults.First(x => x.Name == "DeletePersonSmokeTest");
             var results = stats.Results
                 .Where(x => x.TestRunType == TestRunType.Selenium2)
                 .OrderByDescending(x => x.Time).ToList();
-            Console.WriteLine($"JournalFromCaseSmokeTest: {string.Join(",\n", results)}");
+            Console.WriteLine($"DeletePersonSmokeTest: {string.Join(",\n", results)}");
         }
 
         #endregion
